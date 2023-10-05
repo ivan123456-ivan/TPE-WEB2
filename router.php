b0ec9024cb227dda2108d5b9ef8a1776f79a8111
Iván Calcagno <ivancalcagno3003@gmail.com>
2023-10-04 21:01:28 -0300

    Agregando estructura base del proyecto.

--- router.php
+++ router.php
@@ -1,5 +1,28 @@
 <?php
     /* 
     * Tabla de Ruteo:
-    *
+    * home          -> controller->showHome()
+    * signin        -> controller->signin()
+    * signup        -> controller->signup()
+    * signout       -> controller->signout()
+    * products      -> controller->showProducts($shop)
+    * shops         -> controller->showShops()
+    * 
     */
+
+    require_once './App/controllers/product.controller.php';
+    require_once './App/controllers/shop.controller.php';
+    require_once './App/controllers/user.controller.php';
+
+    define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+    $action = 'home'; // accion por defecto
+    if (!empty( $_GET['action'])) {
+        $action = $_GET['action'];
+    }
+    
+    // parsea la accion Ej: noticia/1 --> ['noticia', 1]
+    $params = explode('/', $action);
+    switch ($params[0]) {
+
+    }
