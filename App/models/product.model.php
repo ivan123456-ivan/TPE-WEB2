bc4c68537afd3d5043c7a194c7fac85117b9df6a
Nicolas Gabriel Sanchez <nicosanchezdeveloper@gmail.com>
2023-10-06 16:09:01 -0300

    Creando registro de producto

--- App/models/product.model.php
+++ App/models/product.model.php
@@ -2,4 +2,16 @@
 require_once './App/models/user.model.php';
 class ProductModel extends UserModel{
 
+
+    public function getProduct(){
+        $productName = $_POST['productName'];
+        $productDescription = $_POST['productDescription'];
+        $productImage = $_POST['productImage'];
+        $productPrice = $_POST['productPrice'];
+        $productStock = $_POST['productStock'];
+    }
+    public function insertNewProduct(){
+        $query = $this->db->prepare("INSERT INTO `productos` (`nombre`, `precio`, `stock`, `id_categoria`, `product_image`, `product_description`) VALUES (?, ?, ?, ?, ?, ?)");
+        $query->execute([$productName, $productPrice, $productStock, $productImage, $productDescription]);
+    }
 }
