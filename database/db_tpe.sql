272e56afce8a5735c33153c65454907e24f9fd1e
Iván Calcagno <ivancalcagno3003@gmail.com>
2023-10-12 19:52:19 -0300

    update database.

--- database/db_tpe.sql
+++ database/db_tpe.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-10-2023 a las 17:35:52
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 13-10-2023 a las 00:51:43
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
   `name` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `categories`
@@ -45,20 +45,6 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `customers`
---
-
-CREATE TABLE `customers` (
-  `id` int(11) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `address` varchar(45) NOT NULL,
-  `user_image` varchar(250) NOT NULL,
-  `id_user` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `products`
 --
 
@@ -68,20 +54,20 @@ CREATE TABLE `products` (
   `price` float NOT NULL,
   `stock` int(11) NOT NULL,
   `id_categories` int(11) NOT NULL,
-  `id_shops` int(11) DEFAULT NULL,
+  `id_shops` int(11) NOT NULL,
   `product_image` varchar(250) NOT NULL,
   `product_description` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `products`
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `stock`, `id_categories`, `id_shops`, `product_image`, `product_description`) VALUES
-(1, 'Pizza de Champiñones', 2500, 24, 1, NULL, 'https://i.pinimg.com/736x/ca/8a/58/ca8a58ca77eb09167b8353886a82c146.jpg', 'Una pizza con productos naturales.'),
-(2, 'Hamburguesa', 2500, 24, 2, NULL, 'https://s3.abcstatics.com/media/gurme/2023/08/31/s/smash-burger.jpg-kbOC--940x529@abc.jpg', 'Esto es una hamburguesa.'),
-(3, 'Empanadas de J&Q', 2500, 24, 3, NULL, 'https://assets.elgourmet.com/wp-content/uploads/2023/03/cover_fpa6sn8vqc_empanadas-1024x683.jpg.webp', 'Empanadas de J&Q muy ricas.'),
-(5, 'Blue Label de Johnnie Walker', 51050, 10, 4, NULL, 'https://dcdn.mitiendanube.com/stores/001/152/625/products/j-walker-blue1-e86b869f1eeeeed48b15882571117532-1024-1024.webp', 'Un Whisky muy sofisticado.');
+(1, 'Pizza de Champiñones', 2500, 24, 1, 6, 'https://i.pinimg.com/736x/ca/8a/58/ca8a58ca77eb09167b8353886a82c146.jpg', 'Una pizza con productos naturales.'),
+(2, 'Hamburguesa', 2500, 24, 2, 6, 'https://s3.abcstatics.com/media/gurme/2023/08/31/s/smash-burger.jpg-kbOC--940x529@abc.jpg', 'Esto es una hamburguesa.'),
+(3, 'Empanadas de J&Q', 2500, 24, 3, 7, 'https://assets.elgourmet.com/wp-content/uploads/2023/03/cover_fpa6sn8vqc_empanadas-1024x683.jpg.webp', 'Empanadas de J&Q muy ricas.'),
+(5, 'Blue Label de Johnnie Walker', 51050, 10, 4, 6, 'https://dcdn.mitiendanube.com/stores/001/152/625/products/j-walker-blue1-e86b869f1eeeeed48b15882571117532-1024-1024.webp', 'Un Whisky muy sofisticado.');
 
 -- --------------------------------------------------------
 
@@ -95,7 +81,15 @@ CREATE TABLE `shops` (
   `address` varchar(45) NOT NULL,
   `shop_image` varchar(250) NOT NULL,
   `id_users` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `shops`
+--
+
+INSERT INTO `shops` (`id`, `name`, `address`, `shop_image`, `id_users`) VALUES
+(6, 'Mostaza', 'Panamá 353', 'https://portalpublicitario.com/wp-content/uploads/2022/05/mostaza-608x608.jpg', 2),
+(7, 'saskdjkl', 'asdasd', 'https://portalpublicitario.com/wp-content/uploads/2022/05/mostaza-608x608.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -106,9 +100,16 @@ CREATE TABLE `shops` (
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `user` varchar(45) NOT NULL,
-  `password` varchar(250) NOT NULL,
-  `rol` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `password` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `user`, `password`) VALUES
+(1, 'webadmin', '$2y$10$MrrNrVH20xLJAxTC6ycQFeWQPakMXCaKK7ntevYRetIlq7n5q7VpO'),
+(2, 'Iván', '$2y$10$bntS.1KV7uUdSlWEvG2gvu5pG91IRheE69D8wdDVQ4Wbq8mGlVyv6');
 
 --
 -- Índices para tablas volcadas
@@ -120,13 +121,6 @@ CREATE TABLE `user` (
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `nombre_UNIQUE` (`name`);
-
---
--- Indices de la tabla `customers`
---
-ALTER TABLE `customers`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_user_idx` (`id_user`);
 
 --
 -- Indices de la tabla `products`
@@ -160,12 +154,6 @@ ALTER TABLE `categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT de la tabla `customers`
---
-ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
@@ -175,25 +163,17 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT de la tabla `shops`
 --
 ALTER TABLE `shops`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
-
 ALTER TABLE `user`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
 --
-
---
--- Filtros para la tabla `customers`
---
-ALTER TABLE `customers`
-  ADD CONSTRAINT `id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `products`
